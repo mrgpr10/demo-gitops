cbdafd798a63ce5489ada9b8aea303e3e17da762
Guruprasad Raikar <gp.raikar@planview.com>
2025-03-20 00:35:19 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>🚀 GitOps Demo (Updated!)</h1>
+      <h1>🚀 GitOps Demo (latest Updated!)</h1>
 
       <p>Welcome to my GitOps-powered frontend!</p>
       <p>Changes here are automatically deployed via GitOps.</p>
